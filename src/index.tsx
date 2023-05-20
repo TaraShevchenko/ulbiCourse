d941dfa6a616986a85c2b834fa72dc4c2611db17
Taras Shevchenko <tarashevchenkowork@gmail.com>
2023-05-21 00:15:18 +0300

    css themes

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,36 @@ import Counter from "./pages/Counter";
 import Main from "./pages/Main";
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import {Suspense} from "react";
+import {useTheme} from "./theme/useTheme";
+import ThemeProvider from "./theme/ThemeProvider";
+import './styles/index.scss';
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <div className="app">
-                <Link to="/">Main</Link>
-                <Link to="/counter">Counter</Link>
+    const {theme, toggleTheme} = useTheme();
 
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route path="/" element={<Main/>}/>
-                        <Route path="/counter" element={<Counter/>}/>
-                    </Routes>
-                </Suspense>
+    return (
+        <div className={`app ${theme}`}>
+            <div>
+                <button onClick={toggleTheme}>Toggle theme</button>
             </div>
-        </BrowserRouter>
+            <Link to="/">Main</Link>
+            <Link to="/counter">Counter</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/counter" element={<Counter/>}/>
+                </Routes>
+            </Suspense>
+        </div>
     );
 }
 
-render(<App/>, document.getElementById('root'));
+render(
+    <BrowserRouter>
+        <ThemeProvider>
+            <App/>
+        </ThemeProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
